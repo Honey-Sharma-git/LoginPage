6ec1b38f5a8dc892ee8f0ae88281fcfa6449ba9c
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-11-16 11:18:01 +0530

    Refactor:
    -Created checkPassword()
    -Added password check confirmation message output
    -Created a button for checking password.
    -Add onclick to current button.
    -Removed <a> and replaced it with js code.
    -Adjusted CSS to keep the style.

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
-console.log('Hello honey sharma');
+
+function checkPassword(){
+    let enteredPassword = document.querySelector('.js-enterPassword-input').value;
+    let reEnteredPassword = document.querySelector('.js-reEnterPassword-input').value;
+    if(enteredPassword === reEnteredPassword){
+        document.querySelector('.js-output').innerHTML = 'Password matched.';
+        window.location.href='formStep3.html';  
+    }else{
+        document.querySelector('.js-output').innerHTML = 'Password did not match.';
+        document.querySelector('.js-reEnterPassword-input').classList.add('wrong-password');
+        document.querySelector('.js-enterPassword-input').classList.add('wrong-password');
+    }
+}
+function resetField(){
+    document.querySelector('.js-enterPassword-input').value = '';
+    document.querySelector('.js-reEnterPassword-input').value = '';
+    document.querySelector('.js-output').innerHTML = '';
+        document.querySelector('.js-reEnterPassword-input').classList.remove('wrong-password');
+        document.querySelector('.js-enterPassword-input').classList.remove('wrong-password');
+}
